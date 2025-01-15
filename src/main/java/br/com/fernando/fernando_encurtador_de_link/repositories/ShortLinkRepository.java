1a70c3c273bd67d54a89eaa4dca1d733bd19ec0e
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2025-01-14 23:02:44 -0300

    Add findByShortLink method in ShortLinkRepository

--- src/main/java/br/com/fernando/fernando_encurtador_de_link/repositories/ShortLinkRepository.java
+++ src/main/java/br/com/fernando/fernando_encurtador_de_link/repositories/ShortLinkRepository.java
@@ -3,7 +3,8 @@ package br.com.fernando.fernando_encurtador_de_link.repositories;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.fernando.fernando_encurtador_de_link.entities.ShortLinkEntity;
+import java.util.Optional;
 
 public interface ShortLinkRepository extends JpaRepository<ShortLinkEntity, UUID>{
-    
+    Optional<ShortLinkEntity> findByShortLink(String shortLink);
 }
