26b0803c25ecc79557e0c31cd288c5425222dc2e
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2025-01-16 16:28:04 -0300

    Create WebMvcConfig config class

---
+++ src/main/java/br/com/fernando/fernando_encurtador_de_link/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package br.com.fernando.fernando_encurtador_de_link.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig {
+    WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
