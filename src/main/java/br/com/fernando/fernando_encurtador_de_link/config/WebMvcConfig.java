94088981e0ff35d31a927219955a7bdde14d3ac6
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2025-01-16 16:38:25 -0300

    Add Bean annotation in webMvcConfigurer bean method

--- src/main/java/br/com/fernando/fernando_encurtador_de_link/config/WebMvcConfig.java
+++ src/main/java/br/com/fernando/fernando_encurtador_de_link/config/WebMvcConfig.java
@@ -1,11 +1,13 @@
 package br.com.fernando.fernando_encurtador_de_link.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig {
+    @Bean
     WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
             @Override
